fa9a566df80780b7a2c882e338117c482a5b4d41
VitoMAC <yohanes.vito@ti.ukdw.ac.id>
2023-03-01 05:03:32 +0700

    fix error

--- vto_odoo_courses/__manifest__.py
+++ vto_odoo_courses/__manifest__.py
@@ -3,24 +3,7 @@
     "version": "14.0.2.0.1",
     "category": "Sales",
     "summary": "New group for seeing only sales channel's documents",
-    "description": """
-    This module aim to manage odoo courses (technical and functional).
-
-    Module structure
-    ==================
-    - Menu:
-        - Odoo Course (with icon)
-            - Courses (list of product courses)
-            - Session (list of session - morning session, night session)
-            - Partners:
-                - Instructors
-                - Attendees
-            - Reporting
-                - Course Analysis
-            - Configuration
-                - Courses
-
-    """,
+    "description": """This module aim to manage odoo courses (technical and functional).""",
     "depends": ["sale", "account"],
     # data harus urut
     "data": [
