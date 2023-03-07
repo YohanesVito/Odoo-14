dc99f6eabfe3ca0e0503b9b88a99131899807ff6
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2023-03-08 01:04:30 +0700

    update controllers

--- vto_odoo_courses/controllers/main.py
+++ vto_odoo_courses/controllers/main.py
@@ -4,8 +4,8 @@ from odoo import fields, http, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 from odoo.addons.payment.controllers.portal import PaymentProcessing
-from odoo.addons.payment.controllers.mail import _message_post_helper
-from odoo.addons.payment.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
 from odoo.osv import expression
 
 class CustomerPortal(CustomerPortal):
@@ -13,8 +13,8 @@ class CustomerPortal(CustomerPortal):
     def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
 
-        if 'course_count' in counters:
-            values['course_count'] = 9999
+        if 'courses_count' in counters:
+            values['courses_count'] = 9999
 
         print("-----counters-----",values)
 
@@ -23,5 +23,14 @@ class CustomerPortal(CustomerPortal):
     @http.route(['/my/courses','/my/courses/page/<int:page>'], type='http', auth='public', website=True)
     def portal_my_courses(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         
-        values = {}
+        courses = request.env['odoo.course'].sudo().search([])
+        course = courses and courses[0] or request.env['odoo.course']
+
+        print('==courses==',courses, '>>>>',course)
+        values = {
+            'page_name': 'courses',
+            'courses': courses,
+            'course':course,
+            'courses_count': len(courses),
+        }
         return request.render["vto_odoo_courses.portal_my_courses",values]
